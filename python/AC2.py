46c12eece78e0ec78696e7fce4e008f5ad1d747a
Douglas Machado <151385@facens.br>
2022-11-22 13:00:28 -0300

    Feito cadastro de horário para motoristas e arrumado erro nas rotas, removido itens não necessario na lista encadeada

--- python/AC2.py
+++ python/AC2.py
@@ -1,6 +1,7 @@
 from ListaEncadeada import *
 onibus = ListaEncadeada()
 motorista = ListaEncadeada()
+motoristaHorarios = ListaEncadeada()
 rotas = ListaEncadeada()
 cobrador = ListaEncadeada()
 listaCheckIn = []
@@ -16,12 +17,12 @@ def cadOnibus():
 
 def cadMotorista():
         print("Cadastro de motorista")
-        infMotorista = input("Informe um nome a ser cadastrado\n")
+        infMotorista = input("Informe um nome a ser cadastrado\n").upper()
         cadDeficiencia = input("O motorista tem alguma deficiência \nque necessita veículo adaptado?\nSim ou não?\n").upper()
         while(not cadDeficiencia == "SIM" and not cadDeficiencia == ("NÃO")):
             cadDeficiencia = input("O motorista tem alguma deficiência \nque necessita veículo adaptado?\nSim ou não?").upper()
-        motorista.insere_no_inicio(motorista, infMotorista)
         motorista.insere_no_inicio(motorista, cadDeficiencia)
+        motorista.insere_no_inicio(motorista, infMotorista)
         print(motorista)
 
 def cadRota():
@@ -29,7 +30,7 @@ def cadRota():
     if(linhaNumDias < 1 or linhaNumDias > 7):
         print("O valor tem que ser superior a 0 e menor que 8")
         return(cadRota())
-    for i in range(linhaNumDias-1):
+    for i in range(linhaNumDias):
         print("1 - Domingo")
         print("2 - Segunda-feira")
         print("3 - Terça-feira")
@@ -87,8 +88,86 @@ def cadRota():
             print("Esta linha já existe em nosso banco de dados")
             linhaNome = input("Informe um código para a linha\n")
         rotas.insere_no_inicio(rotas, linhaNome)
+        if(linhaNumDias == 0):
+            break
     
     print(rotas)
+
+def cadMotoristaHorarios():
+    motoristaBusca = input("Informe o nome do motorista a ser buscado\n").upper()
+    while motorista.busca(motorista, motoristaBusca):    
+        linhaNumDias = int(input("Quantos dias este motorista irá trabalhar?\n"))
+        if(linhaNumDias < 1 or linhaNumDias > 7):
+            print("O valor tem que ser superior a 0 e menor que 8")
+            return(cadRota())
+        for i in range(linhaNumDias):
+            print("1 - Domingo")
+            print("2 - Segunda-feira")
+            print("3 - Terça-feira")
+            print("4 - Quarta-feira")
+            print("5 - Quinta-feira")
+            print("6 - Sexta-feira")
+            print("7 - Sábado")
+            diaJaCad = [None] * 8
+            dia = -1
+            horaInicial = -1
+            horaFinal = -1
+            minutosInicial = -1
+            minutosFinal = -1
+
+            while(True):
+                dia = -1
+                horaInicial = -1
+                horaFinal = -1
+                minutosInicial = -1
+                minutosFinal = -1
+                dia = int(input("Informe um dia da semana para cadastrar entre 1 e 7\n"))
+                while(dia < 1 or dia > 7):
+                    dia = int(input("Informe um dia da semana para cadastrar entre 1 e 7\n"))
+
+                while not diaJaCad[dia] == None:
+                    dia = int(input("Esse dia já foi cadastrado, informe outro dia:\n"))
+                    while dia < 1 or dia > 7:
+                        dia = int(input("Informe um dia válido entre 1 a 7"))                
+                    
+                diaJaCad[dia] = dia
+
+                while(horaInicial < 0 or horaInicial > 23):
+                    horaInicial = int(input("informe o horário inicial de entrada entre 00 a 23\n"))
+                while(minutosInicial < 0 or minutosInicial > 59):
+                    minutosInicial = int(input("Informe um valor para minutos válido\n"))
+                while(horaFinal < 0 or horaFinal > 23):
+                    horaFinal = int(input("informe o horário final de saida entre 00 a 23\n"))
+                while(minutosFinal < 0 or minutosFinal > 59):
+                    minutosFinal = int(input("Informe um valor para minutos válido\n"))
+                Sdia = str(dia)
+                ShoraInicial = str(horaInicial)
+                SminutosInicial  = str(minutosInicial)
+                ShoraFinal = str(horaFinal)
+                SminutosFinal = str(minutosFinal)
+                horaEdia = (Sdia + " " + ShoraInicial + ":" + SminutosInicial + " às " + ShoraFinal + ":" + SminutosFinal)
+
+                motoristaHorarios.insere_no_inicio(motoristaHorarios, horaEdia)
+
+                linhaNumDias = linhaNumDias -1
+                if(linhaNumDias >= 0):
+                    break
+
+        motoristaHorarios.insere_no_inicio(motoristaHorarios, motoristaBusca)
+        
+        print(motoristaHorarios)
+        if(linhaNumDias >= 0):
+            break
+    else:
+        print("Motorista não encontrado")
+        erroBusca = int(input("1 - Cadastrar novo motorista\n2 - Cadastrar horário do motorista\n"))
+        if(erroBusca == 1):
+            cadMotorista()
+        elif(erroBusca == 2):
+            cadMotoristaHorarios
+        else:
+            print("Valor inválido\n")
+
 
 def cadCobrador():
     print("Cadastro de cobrador")
@@ -170,7 +249,7 @@ def desCobrador():
 while True:
     print('Olá usuário o que deseja fazer?')
     print('1 - Cadastrar ônibus')
-    print('2 - Cadastrar motorista')#terminado mas falta testar
+    print('2 - Cadastrar motorista')
     print('3 - Horário de rotas')
     print('4 - Horário para motoristas')
     print('5 - Designar ônibus')
@@ -186,6 +265,8 @@ while True:
         cadMotorista()
     elif(selecaoNum == 3):
         cadRota()
+    elif(selecaoNum == 4):
+        cadMotoristaHorarios()
     elif(selecaoNum == 5):
         desOnibus()
     elif(selecaoNum == 6):
